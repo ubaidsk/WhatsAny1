2449242b9d6fdcdf2638475b7da623ac6b7f3d3b
Ubaid Shaikh <shaikhubaid769@gmail.com>
2025-01-26 00:26:37 +0530

    PWA Support

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        turbo: {}
+    }
+};
 
-export default nextConfig;
+import NextPWA from '@ducanh2912/next-pwa';
+
+const withPWA = NextPWA({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    disable: process.env.NODE_ENV === "development",
+    workboxOptions: {
+      disableDevLogs: true,
+    },
+});
+
+export default withPWA(nextConfig);
